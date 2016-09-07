2cc09c1c3e562f1caf6732db52afcddf6b83a506
josin22 <233679057@qq.com>
2016-09-07 08:51:24 +0800

    de

---
+++ JSWaveDemo/Classes/JSWave.h
@@ -0,0 +1,43 @@
+//
+//  JSWave.h
+//  JSWaveDemo
+//
+//  Created by 乔同新 on 16/8/20.
+//  Copyright © 2016年 乔同新. All rights reserved.
+//  Github  Demo  ::  https://github.com/Josin22/JSWave/
+
+#import <UIKit/UIKit.h>
+
+#define XNColor(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
+
+typedef void(^JSWaveBlock)(CGFloat currentY);
+
+@interface JSWave : UIView
+/**
+ *  浪弯曲度
+ */
+@property (nonatomic, assign) CGFloat waveCurvature;
+/**
+ *  浪速
+ */
+@property (nonatomic, assign) CGFloat waveSpeed;
+/**
+ *  浪高
+ */
+@property (nonatomic, assign) CGFloat waveHeight;
+/**
+ *  实浪颜色
+ */
+@property (nonatomic, strong) UIColor *realWaveColor;
+/**
+ *  遮罩浪颜色
+ */
+@property (nonatomic, strong) UIColor *maskWaveColor;
+
+@property (nonatomic, copy) JSWaveBlock waveBlock;
+
+- (void)stopWaveAnimation;
+
+- (void)startWaveAnimation;
+
+@end
