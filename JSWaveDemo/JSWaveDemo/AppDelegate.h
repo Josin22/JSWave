39171d5f0859ceeb4018a74802d40338ca2bd397
josin22 <233679057@qq.com>
2016-09-07 08:30:23 +0800

    new

---
+++ JSWaveDemo/JSWaveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSWaveDemo
+//
+//  Created by 乔同新 on 16/9/7.
+//  Copyright © 2016年 乔同新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
